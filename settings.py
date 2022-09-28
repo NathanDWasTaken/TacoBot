32d5a2c998b08ce07b75c9874748ddd4d336e3e8
NathanDWasTaken <derna541@hotmail.com>
2022-09-28 17:44:22 +0200

    change Spotipy OAutch cache filename

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ testing                 = False
 
 env_filename            = "secrets.env"
 
-spotify_cache_filename  = ".spotipyoauthcache"
+spotify_cache_filename  = "SpotipyOAuth.cache"
 
 # Stores the shared songs where the last key is the song ID
 shared_songs_by_songID_filename    = "shared_songs_by_songID.json"
